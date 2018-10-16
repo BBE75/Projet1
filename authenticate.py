028c0fec3d16db8a90153a87be7c2ef7b6f7cc76
BenjaminB <beyerle.benjamin@gmail.com>
2018-10-16 14:32:15 +0200

    UPDATE

--- authenticate.py
+++ authenticate.py
@@ -1,4 +1,46 @@
 import csv
+import hashlib
+from getpass import getpass
 
-def authenticate(csv):
-    info = open("~ressources/user.csv", "r")
+
+def password_hash(password):
+    byte_password = password.encode()
+    hashed_password = hashlib.md5(byte_password).hexdigest()
+    return hashed_password
+
+CSV_FILE = 'C:/Users/Benjamin/PycharmProjects/Projet1/ressources/user.csv'
+
+
+def auth():
+
+    data = {}
+    with open(CSV_FILE) as fin:
+        reader = csv.reader(fin, skipinitialspace=True, quotechar="'")
+        for row in reader:
+            data[row[0]] = row[1:]
+    cpt = 0
+    while cpt < 3:
+        username = input('Username (EXIT to exit):')
+        if username not in list(data.keys()):
+            print('user not found')
+            cpt += 1
+            if cpt == 3:
+                print('too many try, closing now')
+                return 0
+        else:
+            break
+    cpt = 0
+    while cpt < 3:
+        password = getpass('Password :', stream=None)
+        if password_hash(password) != data[username][4]:
+            print('Wrong password')
+            cpt += 1
+            if cpt == 3:
+                print('too many try, closing now')
+                return 0
+        else:
+            if data[username][3] == '0':
+                return 2
+            else:
+                return 1
+
--- main.py
+++ main.py
@@ -1,1 +1,9 @@
-print("Hello world")
+import authenticate
+
+value = authenticate.auth()
+if value == 0:
+    print("Authentication failure")
+elif value == 1:
+    print("Successfully logged as user")
+else:
+    print("Successfully logged as admin")
