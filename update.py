5e5aa3d2d124c5fc08f8c776abcd1a6205d9ad69
BenjaminB <beyerle.benjamin@gmail.com>
2018-10-16 17:48:26 +0200

    Setting global variable

--- update.py
+++ update.py
@@ -1,2 +1,9 @@
 import csv
 
+def update(username)
+    data = {}
+    with open(CSV_FILE) as fin:
+        reader = csv.reader(fin, skipinitialspace=True, quotechar="'")
+        for row in reader:
+            data[row[0]] = row[1:]
+
